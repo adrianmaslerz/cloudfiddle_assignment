0128acd8024c7d9f2b913e05f31cfff2618d95eb
Adrian <a.maslerz@dook.pro>
2025-11-14 13:56:13 +0100

    added readme finalization notes + fixed binance service tests setup

--- src/binance/binance.service.spec.ts
+++ src/binance/binance.service.spec.ts
@@ -1,15 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BinanceService } from './binance.service';
+import { HttpModule, HttpService } from '@nestjs/axios';
 
 describe('BinanceService', () => {
   let service: BinanceService;
+  let httpService: HttpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule.register({})],
       providers: [BinanceService],
     }).compile();
 
     service = module.get<BinanceService>(BinanceService);
+    httpService = module.get<HttpService>(HttpService);
   });
 
   it('should be defined', () => {
